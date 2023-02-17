3f2a405b6da6d51d0385a0dbe686376637f5354d
ssokgg <123573598+ssokgg@users.noreply.github.com>
2023-02-17 14:18:42 +0100

    Create ban4.py

---
+++ folder1/ban4.py
@@ -0,0 +1,4 @@
+input: "id=AKIAFJKR45SAWSZ5XDF3, client_secret: hjshnk5ex5u34565d4654HJKGjhz545d89sjkjka"
+output:
+  client_id: AKIAFJKR45SAWSZ5XDF3
+  client_secret: hjshnk5ex5u34565d4654HJKGjhz545d89sjkjka
