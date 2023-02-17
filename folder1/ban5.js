9407322a13f03a028c4d961b84340eb23f5f2b03
ssokgg <123573598+ssokgg@users.noreply.github.com>
2023-02-17 14:19:01 +0100

    Create ban5.js

---
+++ folder1/ban5.js
@@ -0,0 +1,12 @@
+input: |
+  dbusername = admin
+  dbpassword = 8095uohoiw4ur90
+  dbhost = db-postgres-nyc1-1111-do-user-111111-0.db.ondigitalocean.com
+  dbport = 25060
+  dbdatabase = defaultdb
+  dbsslmode = require
+output:
+  username: admin
+  password: 8095uohoiw4ur90
+  host: db-postgres-nyc1-1111-do-user-111111-0.db.ondigitalocean.com
+  port: 25060
